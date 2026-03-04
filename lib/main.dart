a39a3718309dc0c2ea3add5e4585da2cb686cddc
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-04 21:55:04 +0530

    Initial app setup with basic HomeScreen

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gourmet',
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gourmet"),
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to Gourmet App",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
