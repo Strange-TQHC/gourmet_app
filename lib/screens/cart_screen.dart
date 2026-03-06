d66e64f4f14730d80bee2bfb7d4f01f813a2593e
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-06 21:09:47 +0530

    Implemented CartScreen with dynamic cart items and total price

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart_provider.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Cart Screen",
-        style: TextStyle(fontSize: 20),
+
+    final cart = Provider.of<CartProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Cart"),
+      ),
+
+      body: cart.items.isEmpty
+          ? const Center(
+        child: Text("Cart is empty"),
+      )
+          : Column(
+        children: [
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+
+                final item = cart.items[index];
+
+                return ListTile(
+                  title: Text(item.name),
+                  subtitle: Text("₹${item.price}"),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      cart.removeFromCart(item);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              "Total: ₹${cart.totalPrice}",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
