19e4662586892a88d3c66b8aefa5ec80f22f0066
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-12 19:45:46 +0530

    Implemented AuthGate to handle user session using Firebase authStateChanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'screens/auth/login_screen.dart';
 import 'package:provider/provider.dart';
 import 'providers/cart_provider.dart';
 import 'screens/main_screen.dart';
@@ -26,7 +28,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Gourmet',
       debugShowCheckedModeBanner: false,
-      home: const MainScreen(),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+
+      builder: (context, snapshot) {
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (snapshot.hasData) {
+          return const MainScreen();
+        }
+
+        return const LoginScreen();
+      },
     );
   }
 }
