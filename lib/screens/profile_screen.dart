560b188b6ed780c63a3878fb1c80aa019470a8fb
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-12 19:52:06 +0530

    Added logout functionality in ProfileScreen using FirebaseAuth

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Profile Screen",
-        style: TextStyle(fontSize: 20),
+
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+
+      appBar: AppBar(title: const Text("Profile")),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            Text(
+              "Email: ${user?.email}",
+              style: const TextStyle(fontSize: 18),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () async {
+
+                await FirebaseAuth.instance.signOut();
+
+              },
+
+              child: const Text("Logout"),
+            ),
+          ],
+        ),
       ),
     );
   }
