0ccda2f5af930f5953c7ad9e1d7b5ff494143174
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-16 20:10:43 +0530

    Added empty cart UI state

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -16,8 +16,30 @@ class CartScreen extends StatelessWidget {
       ),
 
       body: cart.items.isEmpty
-          ? const Center(
-        child: Text("Cart is empty"),
+          ? Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              Icons.shopping_cart_outlined,
+              size: 80,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 16),
+            Text(
+              "Your cart is empty",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Add something delicious!",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
       )
           : Column(
         children: [
