a7cad8359dd8e666c2f8547c9a54babade5d987f
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-16 18:05:23 +0530

    Created app color palette for consistent UI styling

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xFFFF6B00);
+
+  static const secondary = Color(0xFFFFA726);
+
+  static const background = Color(0xFFF8F8F8);
+
+  static const textDark = Color(0xFF333333);
+
+}
