c475ec319c35d353ed84a1b0bc12e004e85a9281
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-03-19 18:35:02 +0530

    floating transparent capsule navigation bar added

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,6 +5,8 @@ import 'orders_screen.dart';
 import 'profile_screen.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
+import 'dart:ui';
+import '../themes/app_colors.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -27,72 +29,106 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
 
       body: screens[currentIndex],
 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-
-        type: BottomNavigationBarType.fixed,
-
-        items: [
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 20,
+                offset: const Offset(0, 10),
+              ),
+            ],
           ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: BottomNavigationBar(
+                elevation: 0,
+                backgroundColor: Colors.white.withOpacity(0.1),
+                currentIndex: currentIndex,
+                onTap: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                type: BottomNavigationBarType.fixed,
+                selectedIconTheme: const IconThemeData(
+                  size: 30,
+                  color: AppColors.primary,
+                ),
+                unselectedIconTheme: const IconThemeData(
+                  size: 24,
+                  color: Colors.black54,
+                ),
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
 
-          BottomNavigationBarItem(
-            icon: Stack(
-              children: [
+                items: [
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: "Home",
+                  ),
 
-                const Icon(Icons.shopping_cart),
+                  BottomNavigationBarItem(
+                    icon: Stack(
+                      children: [
 
-                Positioned(
-                  right: 0,
-                  child: Consumer<CartProvider>(
-                    builder: (context, cart, child) {
+                        const Icon(Icons.shopping_cart),
 
-                      if (cart.items.isEmpty) {
-                        return const SizedBox();
-                      }
+                        Positioned(
+                          right: 0,
+                          child: Consumer<CartProvider>(
+                            builder: (context, cart, child) {
 
-                      return Container(
-                        padding: const EdgeInsets.all(4),
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Text(
-                          cart.items.length.toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 10,
+                              if (cart.items.isEmpty) {
+                                return const SizedBox();
+                              }
+
+                              return Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: const BoxDecoration(
+                                  color: Colors.red,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Text(
+                                  cart.items.length.toString(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
-                      );
-                    },
+                      ],
+                    ),
+                    label: "Cart",
                   ),
-                ),
-              ],
+
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.receipt),
+                    label: "Orders",
+                  ),
+
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: "Profile",
+                  ),
+                ],
+
+              ),
             ),
-            label: "Cart",
           ),
-
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.receipt),
-            label: "Orders",
-          ),
-
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-          ),
-        ],
+        ),
       ),
     );
   }
